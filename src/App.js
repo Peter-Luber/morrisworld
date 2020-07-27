ecfe497eb27829a4202962c22e526e2e1316b8bc
Peter Luber <prluber@gmail.com>
2020-07-26 18:52:00 -0700

    gallery build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Pic from "./Pic";
+import img1 from "./img/1.jpg";
+import img2 from "./img/2.jpeg";
+import img3 from "./img/3.jpg";
+import img4 from "./img/4.png";
+import img5 from "./img/5.jpg";
+import img6 from "./img/6.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Pic-wrapper">
+        <header className="App-header">morrisworld</header>
+        <div className="Column">
+          <Pic imgUrl={img1} alt="deez nuts" />
+          <Pic imgUrl={img2} alt="deez nuts" />
+          <Pic imgUrl={img3} alt="deez nuts" />
+          <Pic imgUrl={img4} alt="deez nuts" />
+          <Pic imgUrl={img5} alt="deez nuts" />
+          <Pic imgUrl={img6} alt="deez nuts" />
+          <Pic imgUrl={img1} alt="deez nuts" />
+          <Pic imgUrl={img2} alt="deez nuts" />
+          <Pic imgUrl={img3} alt="deez nuts" />
+          <Pic imgUrl={img4} alt="deez nuts" />
+          <Pic imgUrl={img5} alt="deez nuts" />
+          <Pic imgUrl={img6} alt="deez nuts" />
+        </div>
+        <div className="Column">
+          <Pic imgUrl={img6} alt="deez nuts" />
+          <Pic imgUrl={img5} alt="deez nuts" />
+          <Pic imgUrl={img4} alt="deez nuts" />
+          <Pic imgUrl={img3} alt="deez nuts" />
+          <Pic imgUrl={img2} alt="deez nuts" />
+          <Pic imgUrl={img1} alt="deez nuts" />
+          <Pic imgUrl={img6} alt="deez nuts" />
+          <Pic imgUrl={img5} alt="deez nuts" />
+          <Pic imgUrl={img4} alt="deez nuts" />
+          <Pic imgUrl={img3} alt="deez nuts" />
+          <Pic imgUrl={img2} alt="deez nuts" />
+          <Pic imgUrl={img1} alt="deez nuts" />
+        </div>
+        <div className="Column">
+          <Pic imgUrl={img2} alt="deez nuts" />
+          <Pic imgUrl={img4} alt="deez nuts" />
+          <Pic imgUrl={img6} alt="deez nuts" />
+          <Pic imgUrl={img1} alt="deez nuts" />
+          <Pic imgUrl={img3} alt="deez nuts" />
+          <Pic imgUrl={img5} alt="deez nuts" />
+          <Pic imgUrl={img2} alt="deez nuts" />
+          <Pic imgUrl={img4} alt="deez nuts" />
+          <Pic imgUrl={img6} alt="deez nuts" />
+          <Pic imgUrl={img1} alt="deez nuts" />
+          <Pic imgUrl={img3} alt="deez nuts" />
+          <Pic imgUrl={img5} alt="deez nuts" />
+        </div>
+        <div className="Column">
+          <Pic imgUrl={img3} alt="deez nuts" />
+          <Pic imgUrl={img2} alt="deez nuts" />
+          <Pic imgUrl={img1} alt="deez nuts" />
+          <Pic imgUrl={img6} alt="deez nuts" />
+          <Pic imgUrl={img5} alt="deez nuts" />
+          <Pic imgUrl={img4} alt="deez nuts" />
+          <Pic imgUrl={img3} alt="deez nuts" />
+          <Pic imgUrl={img2} alt="deez nuts" />
+          <Pic imgUrl={img1} alt="deez nuts" />
+          <Pic imgUrl={img6} alt="deez nuts" />
+          <Pic imgUrl={img5} alt="deez nuts" />
+          <Pic imgUrl={img4} alt="deez nuts" />
+        </div>
+      </div>
     </div>
   );
 }
